740b8ade4ace13aac8f03a45e159d7980ed76f61
Nick <nisergeev_1@edu.hse.ru>
2022-03-28 15:47:56 +0300

    Исправил Пузырьковую Сортировку

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,18 +5,20 @@ from random import randint
 def bubble_sort(lst):
     swapped = True
     while swapped:
+        swapped = False
         for i in range(len(lst) - 1):
             if lst[i] > lst[i + 1]:
                 lst[i], lst[i + 1] = lst[i + 1], lst[i]
-                swapped = False
-        if swapped:
-            break
+                swapped = True
     return lst
 
 
-ARRAY_LENGTH = 10000
+ARRAY_LENGTH = 100
 
 if __name__ == "__main__":
     array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+    print(array)
+    bubble_sort(array)
+    print(array)
 
     time(algorithm="bubble_sort", array=array)
